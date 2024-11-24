1ff399b1cc8b845766f762325a94576aa2ac0268
tindt <18420952+tindt4869@users.noreply.github.com>
2024-11-24 18:52:02 +0700

    feat(contact): send email

--- js/20810233.js
+++ js/20810233.js
@@ -1,4 +1,5 @@
 const API = "https://web1-api.vercel.app/api";
+const AUTHENTICATE_API = "https://web1-api.vercel.app/users";
 
 async function loadData(request, templateId, viewId) {
   const response = await fetch(`${API}/${request}`);
@@ -9,4 +10,23 @@ async function loadData(request, templateId, viewId) {
   const context = { data };
   const view = document.getElementById(viewId);
   view.innerHTML = template(context);
+}
+
+async function getAuthenticationToken(username, password) {
+  let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  let result = await response.json();
+
+  if (result.status === 200) {
+    return result.token;
+  }
+
+  throw new Error(result.message);
 }
