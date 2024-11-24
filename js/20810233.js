7852b245a4994b0a20e53f320f482f436ea3d355
tindt <18420952+tindt4869@users.noreply.github.com>
2024-11-24 19:39:55 +0700

    feat(login): add login, logout for all pages

--- js/20810233.js
+++ js/20810233.js
@@ -30,3 +30,71 @@ async function getAuthenticationToken(username, password) {
 
   throw new Error(result.message);
 }
+
+async function login(event) {
+  event.preventDefault();
+
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  try {
+    let token = await getAuthenticationToken(username, password);
+    if (token) {
+      localStorage.setItem("token", token);
+      document.getElementsByClassName("btn-close")[0].click();
+      displayControls();
+    }
+  } catch (error) {
+    document.getElementById("errorMessage").innerHTML = error;
+    displayControls(false);
+  }
+}
+
+function displayControls(isLogin = true) {
+  let linkLogin = document.getElementsByClassName("linkLogin");
+  let linkLogout = document.getElementsByClassName("linkLogout");
+
+  let displayLogin = "none";
+  let displayLogout = "block";
+
+  if (!isLogin) {
+    displayLogin = "block";
+    displayLogout = "none";
+  }
+
+  for (let i = 0; i < 2; i++) {
+    linkLogin[i].style.display = displayLogin;
+    linkLogout[i].style.display = displayLogout;
+  }
+}
+
+async function checkLogin() {
+  let isLogin = await verifyToken();
+  displayControls(isLogin);
+}
+
+async function verifyToken() {
+  let token = localStorage.getItem("token");
+
+  if (token) {
+    let response = await fetch(`${AUTHENTICATE_API}/verify`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+
+    if (response.status === 200) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function logout() {
+  localStorage.clear();
+  displayControls(false);
+}
